3481cbb779383b4701607c4cdb868986661fef3f
Vitor Silva <vitor.dasilvacruz@gmail.com>
2017-12-27 15:05:56 -0200

    comita

--- script.js
+++ script.js
@@ -38,7 +38,7 @@
             for (let i = 0; i < buffer.byteLength; i++) {
                 bytes[i] = binary.charCodeAt(i) & 0xFF
             }
-            let blob = new Blob([bytes], type)
+            let blob = new Blob([bytes], {type:'audio/mp3'})
             object.href = window.URL.createObjectURL(blob)
             object.download = Math.random()*1000+'.ogg'
             // window.URL.revokeObjectURL(object.href)
